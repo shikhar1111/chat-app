8c66d057971d5578b050b91e4e7a00a19e3e5757
shikhar1111 <shikhar4frnds.agarwal11@gmail.com>
2019-06-29 13:58:34 +0530

    chat-app

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000')
+const socket = io('')
 const messageContainer = document.getElementById('message-container')
 const groupContainer = document.getElementById('group-container')
 const messageform = document.getElementById('send-container')
